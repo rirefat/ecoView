0114f4eed0f38f339423e02b79e1d588d91e7155
rirefat <rirefat.official@gmail.com>
2024-10-03 04:41:57 +0600

    solve issue

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import LocationDetector from "@/components/LocationDetector";
+import { Suspense } from "react";
 
 export default function Home() {
-    return <LocationDetector />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LocationDetector />
+    </Suspense>
+  );
 }
