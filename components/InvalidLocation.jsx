dd020389ba9670f064326ce682b1768ca716f96a
rirefat <rirefat.official@gmail.com>
2024-10-06 06:03:55 +0600

    complete InvalidLocation component

--- components/InvalidLocation.jsx
+++ components/InvalidLocation.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Card from './Card';
+import Link from 'next/link';
 
 const InvalidLocation = () => {
     return (
-        <div>
-            Not found location info...
-        </div>
+        <Card>
+            <h4 className='text-center my-5'>Not found location info.</h4>
+            <Link
+                href={'/'}
+                className="underline text-center block text-gray-400 hover:text-gray-200"
+            >
+                Try Again
+            </Link>
+        </Card>
     );
 };
 
